a63734ff42643b6abf2fd9098496599a67e57315
kackerzzz <rishabhee21@gmail.com>
2020-05-20 06:37:13 +0530

    Added Data via JDBC insert

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.pluralsight.repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -12,23 +15,38 @@ import com.pluralsight.model.Ride;
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-	@Override
-	public List<Ride> getRides() {
-		Ride ride = new Ride();
-		ride.setName("Corner Canyon");
-		ride.setDuration(120);
-		List <Ride> rides = new ArrayList<>();
-		rides.add(ride);
-		return rides;
-	}
+    @Override
+    public List<Ride> getRides() {
+        Ride ride = new Ride();
+        ride.setName("Corner Canyon");
+        ride.setDuration(120);
+        List<Ride> rides = new ArrayList<>();
+        rides.add(ride);
+        return rides;
+    }
 
-	@Override
-	public Ride createRide(Ride ride) {
-		jdbcTemplate.update("insert into Ride (name , duration) values ( ?, ? ) ", ride.getName(), ride.getDuration());
-		return null;
-	}
+    @Override
+    public Ride createRide(Ride ride) {
+//		jdbcTemplate.update("insert into Ride (name , duration) values ( ?, ? ) ", ride.getName(), ride.getDuration());
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+        List<String> columns = new ArrayList<>();
+
+        columns.add("name");
+        columns.add("duration");
+
+        insert.setTableName("Ride");
+        insert.setColumnNames(columns);
+
+        Map<String , Object> data = new HashMap<>();
+        data.put("name", ride.getName());
+        data.put("duration", ride.getDuration());
+        insert.setGeneratedKeyName("id");
+        Number key =  insert.executeAndReturnKey(data);
+        System.out.println(key.toString());
+        return null;
+    }
 
 }
--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -17,8 +17,8 @@ public class RestControllerTest {
 	public void testCreateRides() {
 		RestTemplate restTemplate = new RestTemplate();
 		Ride ride = new Ride();
-		ride.setName("Trail");
-		ride.setDuration(10);
+		ride.setName("Death well");
+		ride.setDuration(20);
 		restTemplate.put("http://localhost:8080/ride_tracker/rides", ride);
 	}
 
